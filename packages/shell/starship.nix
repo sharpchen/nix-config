edf9dcb9e1bf0547d79a8133ed9e70b9fa023456
sharpchen <rui.chen.sharp@gmail.com>
2024-10-13 20:37:07 +0800

    fix(starship): use nix options

--- packages/shell/starship.nix
+++ packages/shell/starship.nix
@@ -1,11 +1,30 @@
-{pkgs, ...}:
 {
-  home.packages = with pkgs;[
-    starship
-  ];
-
-  xdg.configFile."startship.toml" = {
-    source = ../dotfiles/starship.toml;
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    add_newline = true;
+    shlvl = {
+      disabled = false;
+      symbol = "ﰬ";
+      style = "bright-red bold";
+    };
+    shell = {
+      disabled = false;
+      format = "$indicator";
+      fish_indicator = "";
+      bash_indicator = "[BASH](bright-white) ";
+    };
+    username = {
+      style_user = "bright-white bold";
+      style_root = "bright-red bold";
+    };
+    dotnet = {
+      symbol  = ".NET SDK: ";
+      format = " [$symbol($version )(targets $tfm )]($style)";
+    };
+    git_branch = {
+      only_attached = true;
+      format = "[$symbol$branch]($style) ";
+      symbol = " ";
+    };
   };
-
 }
