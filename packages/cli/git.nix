7edb21495989def34021cc46d922cf90d0d44e78
sharpchen <rui.chen.sharp@gmail.com>
2024-10-18 15:19:28 +0800

    fix(git): use raw file for .gitconfig

--- packages/cli/git.nix
+++ packages/cli/git.nix
@@ -1,14 +1,4 @@
 {
   programs.git.enable = true;
-  home.file.".gitconfig" = {
-    text = /* gitconfig */''
-      [user]
-        name = sharpchen
-        email = rui.chen.sharp@gmail.com
-      [init]
-        defaultBranch = main
-      [url "git@github.com:"]
-        insteadOf = https://github.com/
-    '';
-  };
+  home.file.".gitconfig".source = ../../dotfiles/.gitconfig;
 }
