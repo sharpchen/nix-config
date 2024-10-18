ccb14c8eb8661b50d0546132badf8bcfd7e608c5
sharpchen <rui.chen.sharp@gmail.com>
2024-10-18 15:20:19 +0800

    fix(vim): rebuild for clipboard support

--- packages/cli/vim.nix
+++ packages/cli/vim.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    vim
+  home.packages = [
+    (pkgs.vim.overrideAttrs (oldAttrs: {
+      buildInputs = (oldAttrs.buildInputs ++ [ pkgs.xorg.libX11 pkgs.xorg.libXt ]);
+    }))
   ];
+
   home.file.".vimrc".source = ../../dotfiles/.vimrc;
   home.sessionVariables = {
     MYVIMRC = "~/.vimrc";
