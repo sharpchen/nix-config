9efa821d9eef0658b00ba0091305fbf5d7424cfd
sharpchen <rui.chen.sharp@gmail.com>
2024-10-18 18:38:50 +0800

    chore(windows): add config.nu

--- windows.ps1
+++ windows.ps1
@@ -4,6 +4,7 @@ echo "Starting restoring dotfiles for windows..."
 New-Item -ItemType SymbolicLink -Path "~/.gitconfig" -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.gitconfig")) -Force
 New-Item -ItemType SymbolicLink -Path "~/.vsvimrc" -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.vsvimrc")) -Force
 New-Item -ItemType SymbolicLink -Path "~/.vimrc" -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.vimrc")) -Force
-New-Item -ItemType SymbolicLink -Path "~/.wezterm" -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.wezterm")) -Force
+New-Item -ItemType SymbolicLink -Path "~/.wezterm.lua" -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.wezterm.lua")) -Force
+New-Item -ItemType SymbolicLink -Path $([System.IO.Path]::Combine(($env.APPDATA), "nushell/config.nu")) -Target $([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/config.nu")) -Force
 
 echo "Restore finished."
