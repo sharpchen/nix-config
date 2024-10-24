b2675ae56b77f2c85cf61ac08e191af6081563dc
sharpchen <rui.chen.sharp@gmail.com>
2024-10-24 15:11:45 +0800

    chore(windows): use full function name instead

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -1,5 +1,5 @@
 # this file is for restoring dotfiles on windows
-echo "Starting restoring dotfiles for windows..."
+Write-Output "Starting restoring dotfiles for windows..."
 
 New-Item -ItemType SymbolicLink -Path "~/.gitconfig" -Target ([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.gitconfig")) -Force
 New-Item -ItemType SymbolicLink -Path "~/.vsvimrc" -Target ([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/.vsvimrc")) -Force
@@ -8,4 +8,4 @@ New-Item -ItemType SymbolicLink -Path "~/.wezterm.lua" -Target $([System.IO.Path
 New-Item -ItemType SymbolicLink -Path ([System.IO.Path]::Combine($env:APPDATA, "nushell/config.nu")) -Target ([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/config.nu")) -Force
 New-Item -ItemType SymbolicLink -Path ([System.IO.Path]::Combine($env:LOCALAPPDATA, "lazygit/config.yml")) -Target ([System.IO.Path]::Combine((Get-Location).Path, "dotfiles/lazygit.config.yml")) -Force
 
-echo "Restore finished."
+Write-Output "Restore finished."
