7b2e8df698d216b1fed612bbb2ffe4b9490a2942
sharpchen <rui.chen.sharp@gmail.com>
2024-11-11 22:25:01 +0800

    feat(pwsh): enable vi mode

--- dotfiles/pwsh.profile.ps1
+++ dotfiles/pwsh.profile.ps1
@@ -1,3 +1,18 @@
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Install-Module -Name PSReadLine -Force -Scope CurrentUser
+}
+
+if ($IsWindows) {
+    if (-not (Get-Module -ListAvailable -Name Microsoft.WinGet.CommandNotFound)) {
+        Install-Module -Name Microsoft.WinGet.CommandNotFound -Force -Scope CurrentUser
+    }
+    Import-Module -Name Microsoft.WinGet.CommandNotFound
+}
+
+Import-Module PSReadLine -ErrorAction SilentlyContinue
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ViModeIndicator Cursor
 Set-PSReadlineKeyHandler -Key "Ctrl+ " -Function MenuComplete
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
@@ -11,6 +26,3 @@ function :q {
     exit
 }
 
-if ($IsWindows) {
-    Import-Module -Name Microsoft.WinGet.CommandNotFound
-}
