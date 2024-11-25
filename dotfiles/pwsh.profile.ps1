be7323dd84c42ca2e65a01671c2a1a6461d2d44c
sharpchen <rui.chen.sharp@gmail.com>
2024-11-26 00:11:22 +0800

    chore(pwsh): update profile

--- dotfiles/pwsh.profile.ps1
+++ dotfiles/pwsh.profile.ps1
@@ -7,12 +7,36 @@ if ($IsWindows) {
         Install-Module -Name Microsoft.WinGet.CommandNotFound -Force -Scope CurrentUser
     }
     Import-Module -Name Microsoft.WinGet.CommandNotFound
+
+    ## project search
+    function proj {
+        sl (gci '~/Projects' -Directory | foreach FullName | fzf)
+    }
 }
 
 Import-Module PSReadLine -ErrorAction SilentlyContinue
 
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -ViModeIndicator Cursor
+Set-PSReadLineOption -ViModeIndicator Prompt # Cursor
+
+$syntaxColors = @{
+    Parameter = 'Magenta'
+    Operator = 'Cyan'
+    Type = 'Cyan'
+    Keyword = 'Magenta'
+    Command = 'Blue'
+    Number = 'Yellow'
+    Member = 'Red'
+}
+
+if ((gmo -Name PSReadLine).Version -lt '2.0.0') {
+    $syntaxColors.Keys | foreach {
+        Set-PSReadlineOption -TokenKind $_ -ForegroundColor $syntaxColors[$_]
+    }
+} else {
+    Set-PSReadLineOption -Colors $syntaxColors
+}
+
 Set-PSReadlineKeyHandler -Key "Ctrl+ " -Function MenuComplete
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
@@ -20,7 +44,7 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
 
-New-Alias lg lazygit
+Set-Alias lg lazygit
 
 function :q {
     exit
