706ded7e4dfd84fb84c8ac70fa0927f42a309cbe
sharpchen <rui.chen.sharp@gmail.com>
2024-12-11 18:36:40 +0800

    chore(windows): clean script

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -1,31 +1,31 @@
 # this file is for restoring dotfiles on windows
 
-if (Get-Command scoop) {
-    $scoopRoot = [IO.Path]::GetDirectoryName([IO.Path]::GetDirectoryName((Get-Command scoop).Source))
+if (gcm scoop) {
+    $scoopRoot = [IO.Path]::GetDirectoryName([IO.Path]::GetDirectoryName((gcm scoop).Source))
     # librewolf overrides
-    New-Item -Path ([IO.Path]::Combine($scoopRoot, 'persist/librewolf/Profiles/Default/librewolf.overrides.cfg')) -Target ([IO.Path]::Combine($pwd, "dotfiles/librewolf.cfg")) -ItemType SymbolicLink -Force
+    ni ([IO.Path]::Combine($scoopRoot, 'persist/librewolf/Profiles/Default/librewolf.overrides.cfg')) -Target ([IO.Path]::Combine($pwd, "dotfiles/librewolf.cfg")) -ItemType SymbolicLink -Force
     [Environment]::SetEnvironmentVariable('YAZI_FILE_ONE', ([IO.Path]::Combine($scoopRoot, 'apps\git\current\usr\bin\file.exe')), 'User')
 }
 
 # git
-New-Item -Path "~/.gitconfig" -Target ([IO.Path]::Combine($pwd, "dotfiles/.gitconfig")) -ItemType SymbolicLink -Force
-New-Item -Path "~/.gitconfig_windows" -Target ([IO.Path]::Combine($pwd, "dotfiles/.gitconfig-windows")) -ItemType SymbolicLink -Force
-New-Item -Path ([IO.Path]::Combine($env:LOCALAPPDATA, "lazygit/config.yml")) -Target ([IO.Path]::Combine($pwd, "dotfiles/lazygit.config.yml")) -ItemType SymbolicLink -Force
+ni "~/.gitconfig" -Target ([IO.Path]::Combine($pwd, "dotfiles/.gitconfig")) -ItemType SymbolicLink -Force
+ni "~/.gitconfig_windows" -Target ([IO.Path]::Combine($pwd, "dotfiles/.gitconfig-windows")) -ItemType SymbolicLink -Force
+ni ([IO.Path]::Combine($env:LOCALAPPDATA, "lazygit/config.yml")) -Target ([IO.Path]::Combine($pwd, "dotfiles/lazygit.config.yml")) -ItemType SymbolicLink -Force
 
 # vim
-New-Item -Path "~/.vimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.vimrc")) -ItemType SymbolicLink -Force
-New-Item -Path "~/.vsvimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.vsvimrc")) -ItemType SymbolicLink -Force
-New-Item -Path "~/.ideavimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.ideavimrc")) -ItemType SymbolicLink -Force
+ni "~/.vimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.vimrc")) -ItemType SymbolicLink -Force
+ni "~/.vsvimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.vsvimrc")) -ItemType SymbolicLink -Force
+ni "~/.ideavimrc" -Target ([IO.Path]::Combine($pwd, "dotfiles/.ideavimrc")) -ItemType SymbolicLink -Force
 
 # shell
-New-Item -Path '~/.bashrc' -Target ([IO.Path]::Combine($pwd, "dotfiles/.bashrc")) -ItemType SymbolicLink -Force
-New-Item -Path $Profile -Target ([IO.Path]::Combine($pwd, "dotfiles/pwsh.profile.ps1")) -ItemType SymbolicLink -Force
-New-Item -Path ([IO.Path]::Combine($env:APPDATA, "nushell/config.nu")) -Target ([IO.Path]::Combine($pwd, "dotfiles/config.nu")) -ItemType SymbolicLink -Force
-New-Item -Path "~/.config/wezterm/wezterm.lua" -Target ([IO.Path]::Combine($pwd, "dotfiles/.wezterm.lua")) -ItemType SymbolicLink -Force
+ni '~/.bashrc' -Target ([IO.Path]::Combine($pwd, "dotfiles/.bashrc")) -ItemType SymbolicLink -Force
+ni $Profile -Target ([IO.Path]::Combine($pwd, "dotfiles/pwsh.profile.ps1")) -ItemType SymbolicLink -Force
+ni ([IO.Path]::Combine($env:APPDATA, "nushell/config.nu")) -Target ([IO.Path]::Combine($pwd, "dotfiles/config.nu")) -ItemType SymbolicLink -Force
+ni "~/.config/wezterm/wezterm.lua" -Target ([IO.Path]::Combine($pwd, "dotfiles/.wezterm.lua")) -ItemType SymbolicLink -Force
 
 # yazi
-New-Item -Path ([IO.Path]::Combine($env:APPDATA, 'yazi/config/yazi.toml')) -Target ([IO.Path]::Combine($pwd, 'dotfiles/yazi.toml')) -ItemType SymbolicLink -Force
-New-Item -Path ([IO.Path]::Combine($env:APPDATA, 'yazi/config/keymap.toml')) -Target ([IO.Path]::Combine($pwd, 'dotfiles/yazi.keymap.toml')) -ItemType SymbolicLink -Force
+ni ([IO.Path]::Combine($env:APPDATA, 'yazi/config/yazi.toml')) -Target ([IO.Path]::Combine($pwd, 'dotfiles/yazi.toml')) -ItemType SymbolicLink -Force
+ni ([IO.Path]::Combine($env:APPDATA, 'yazi/config/keymap.toml')) -Target ([IO.Path]::Combine($pwd, 'dotfiles/yazi.keymap.toml')) -ItemType SymbolicLink -Force
 if (gcm ya) {
     ya pack -a 'Reledia/glow'
     ya pack -a 'ndtoan96/ouch'
