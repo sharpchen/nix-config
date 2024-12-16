5bb31928f21423c8b294aeca3e8bde873bf51a69
sharpchen <rui.chen.sharp@gmail.com>
2024-12-17 02:24:41 +0800

    fix(pwsh_es): remove unnecessary config in derivation

--- packages/language-server/pwsh_ls/default.nix
+++ packages/language-server/pwsh_ls/default.nix
@@ -3,7 +3,6 @@
   fetchzip,
   lib,
   powershell,
-  makeWrapper,
   bash,
 }:
 let
@@ -15,37 +14,31 @@ stdenvNoCC.mkDerivation rec {
   version = "4.1.0";
 
   src = fetchzip {
-    name = "${repo}";
+    name = repo;
     url = "https://github.com/${owner}/${repo}/releases/download/v${version}/${repo}.zip";
     hash = "sha256-B6RF4RoJB+C5i6puZhfy6FZzyZ9eMo81dd0XsaIEK6Q=";
     stripRoot = false;
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [
-  ];
-
   installPhase = ''
     mkdir -p $out/{lib,bin}
-    mv * $out/lib/
-    cat > $out/bin/${pname} <<EOF
+    mkdir -p $out/lib/powershell-editor-services/
+    mv * $out/lib/powershell-editor-services/
+    cat > $out/bin/powershell-editor-services <<EOF
     #! ${bash}/bin/bash -e
-    exec "${powershell}/bin/pwsh" -noprofile -nologo -c "$out/lib/${repo}/Start-EditorServices.ps1 \$@"
+    exec "${powershell}/bin/pwsh" -noprofile -nologo -c "$out/lib/PowerShellEditorServices/Start-EditorServices.ps1 \$@"
     EOF
     chmod +x $out/bin/*
   '';
 
-  postInstall = ''
-    wrapProgram $out/bin/${pname} --set PATH "$PATH:$out/bin"
-  '';
-
-  meta = {
-    description = "A common platform for PowerShell development support in any editor or application!";
+  meta = with lib; {
+    description = "Common platform for PowerShell development support in any editor or application";
     homepage = "https://github.com/PowerShell/PowerShellEditorServices";
     changelog = "https://github.com/PowerShell/PowerShellEditorServices/releases/tag/v${version}";
-    platforms = [ "x86_64-linux" ];
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ sharpchen ];
-    mainProgram = pname;
+    platforms = platforms.unix;
+    license = licenses.mit;
+    maintainers = with maintainers; [ sharpchen ];
+    mainProgram = "powershell-editor-services";
+    sourceProvenance = with sourceTypes; [ binaryBytecode ];
   };
 }
