dd060092e5f6c27f6fac378a2e9a766cf1aefa0b
sharpchen <rui.chen.sharp@gmail.com>
2024-12-30 20:22:23 +0800

    chore(pwsh): add new keymaps and functions

--- dotfiles/pwsh.profile.ps1
+++ dotfiles/pwsh.profile.ps1
@@ -62,12 +62,15 @@ if ((Get-Module -Name PSReadLine).Version -lt '2.0.0') {
     Set-PSReadLineOption -Colors $syntaxColors
 }
 
-Set-PSReadLineKeyHandler -Key 'Ctrl+ ' -Function MenuComplete
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key 'Ctrl+ ' -Function MenuComplete
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord ' , ' -Function DeleteWord -ViMode Command
+Set-PSReadlineKeyHandler -Chord 'g,h' -Function GotoFirstNonBlankOfLine -ViMode Command
+Set-PSReadlineKeyHandler -Chord 'g,l' -Function EndofLine -ViMode Command
 
 Set-Alias lg lazygit
 Set-Alias dn dotnet
@@ -78,6 +81,31 @@ function vim {
 
 function :q {
     exit
+}
+
+function adbin([string]$Str, [switch]$Enter) {
+    $special = @( ' ', '\|', '\$', '&', '\(', '\)', '~', '\*', "\'", '"', '<', '>')
+    foreach ($char in $special) {
+        $Str = $Str -replace $char, ($char.Length -gt 1 ? $char : "\$char")
+    }
+    adb shell input text $Str
+    if ($Enter) {
+        adb shell input keyevent KEYCODE_ENTER
+    }
+}
+
+function iparam {
+    param (
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [string]$Command,
+        [switch]$Positional,
+        [switch]$Pipeline,
+    )
+    if ($Positional) {
+        help $Command | Select-String 'Position\??\s*\d' -Context 3, 5
+    } elseif ($Pipeline) {
+        help $Command | Select-String 'Accept pipeline input\??\s*true.*$' -Context 5, 4
+    }
 }
 
 if ((Get-Command 'home-manager' -ErrorAction SilentlyContinue)) {
