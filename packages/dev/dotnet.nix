65b2a4d1110cecb83bd4f667a13e94257d95b6c8
sharpchen <rui.chen.sharp@gmail.com>
2025-01-07 22:18:41 +0800

    chore(dotnet): update to dotnet9

--- packages/dev/dotnet.nix
+++ packages/dev/dotnet.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    dotnetCorePackages.sdk_8_0_1xx
+    # dotnetCorePackages.sdk_8_0_1xx
+    dotnetCorePackages.sdk_9_0
     netcoredbg
     csharpier
     roslyn-ls
