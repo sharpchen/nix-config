a6eacf38bc6e7544b895746040e1ed451d9120dd
sharpchen <rui.chen.sharp@gmail.com>
2025-06-07 07:57:25 +0800

    chore(wezterm): reformat config

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm') --[[@as Wezterm]]
 
 local config = wezterm.config_builder()
 
 config.front_end = 'OpenGL'
-config.color_scheme = "kanagawabones"
+config.color_scheme = 'kanagawabones'
 
-config.default_prog = { "pwsh" }
+config.default_prog = { 'pwsh' }
 
 config.animation_fps = 60
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
@@ -22,137 +22,147 @@ config.cursor_blink_ease_out = 'Constant'
 
 config.hide_tab_bar_if_only_one_tab = true
 
-local best_fonts = { 'JetBrains Mono NL', 'SF Mono', 'Cascadia Mono', 'IBM Plex Mono', 'Roboto Mono' }
+local best_fonts = {
+  'JetBrains Mono NL',
+  'SF Mono',
+  'Cascadia Mono',
+  'IBM Plex Mono',
+  'Roboto Mono',
+}
 math.randomseed(os.time())
 local font = best_fonts[math.random(#best_fonts)]
 
 config.font_size = 12
-config.font =
-	wezterm.font_with_fallback({ font, "Nerd Font Symbols", "Noto Color Emoji", "Symbols Nerd Font Mono" })
+config.font = wezterm.font_with_fallback {
+  font,
+  'Nerd Font Symbols',
+  'Noto Color Emoji',
+  'Symbols Nerd Font Mono',
+}
 config.font_rules = {
-	{
-		intensity = "Bold",
-		italic = false,
-		font = wezterm.font(font, { weight = "Bold" }),
-	},
+  {
+    intensity = 'Bold',
+    italic = false,
+    font = wezterm.font(font, { weight = 'Bold' }),
+  },
 }
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
-wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-	local prefix = ""
+wezterm.on('update-right-status', function(window, _)
+  local SOLID_LEFT_ARROW = ''
+  local ARROW_FOREGROUND = { Foreground = { Color = '#c6a0f6' } }
+  local prefix = ''
 
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	end
+  if window:leader_is_active() then
+    prefix = ' ' .. utf8.char(0x1f30a) -- ocean wave
+    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  end
 
-	if window:active_tab():tab_id() ~= 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-	end -- arrow color based on if tab is first pane
+  if window:active_tab():tab_id() ~= 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = '#1e2030' } }
+  end -- arrow color based on if tab is first pane
 
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Text = prefix },
-		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
-	}))
+  window:set_left_status(wezterm.format {
+    { Background = { Color = '#b7bdf8' } },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW },
+  })
 end)
 
 local act = wezterm.action
-config.leader = { key = "a", mods = "CTRL", timeout_miliseconds = 2000 }
+config.leader = { key = 'a', mods = 'CTRL', timeout_miliseconds = 2000 }
 config.keys = {
-	{
-		mods = "LEADER",
-		key = "a",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		mods = "LEADER",
-		key = "c",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		mods = "LEADER",
-		key = "b",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		mods = "LEADER",
-		key = "n",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		mods = "LEADER",
-		key = "\\",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "h",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		mods = "LEADER",
-		key = "j",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		mods = "LEADER",
-		key = "k",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		mods = "LEADER",
-		key = "l",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		mods = "LEADER",
-		key = "LeftArrow",
-		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "RightArrow",
-		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "DownArrow",
-		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "UpArrow",
-		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-	},
+  {
+    mods = 'LEADER',
+    key = 'a',
+    action = wezterm.action.SpawnTab('CurrentPaneDomain'),
+  },
+  {
+    mods = 'LEADER',
+    key = 'c',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    mods = 'LEADER',
+    key = 'b',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    mods = 'LEADER',
+    key = 'n',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    mods = 'LEADER',
+    key = '\\',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'LEADER',
+    key = '-',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'LEADER',
+    key = 'h',
+    action = wezterm.action.ActivatePaneDirection('Left'),
+  },
+  {
+    mods = 'LEADER',
+    key = 'j',
+    action = wezterm.action.ActivatePaneDirection('Down'),
+  },
+  {
+    mods = 'LEADER',
+    key = 'k',
+    action = wezterm.action.ActivatePaneDirection('Up'),
+  },
+  {
+    mods = 'LEADER',
+    key = 'l',
+    action = wezterm.action.ActivatePaneDirection('Right'),
+  },
+  {
+    mods = 'LEADER',
+    key = 'LeftArrow',
+    action = wezterm.action.AdjustPaneSize { 'Left', 5 },
+  },
+  {
+    mods = 'LEADER',
+    key = 'RightArrow',
+    action = wezterm.action.AdjustPaneSize { 'Right', 5 },
+  },
+  {
+    mods = 'LEADER',
+    key = 'DownArrow',
+    action = wezterm.action.AdjustPaneSize { 'Down', 5 },
+  },
+  {
+    mods = 'LEADER',
+    key = 'UpArrow',
+    action = wezterm.action.AdjustPaneSize { 'Up', 5 },
+  },
 }
 config.mouse_bindings = {
-	{
-		event = { Down = { streak = 1, button = "Right" } },
-		mods = "NONE",
-		action = wezterm.action_callback(function(window, pane)
-			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-			if has_selection then
-				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-				window:perform_action(act.ClearSelection, pane)
-			else
-				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-			end
-		end),
-	},
+  {
+    event = { Down = { streak = 1, button = 'Right' } },
+    mods = 'NONE',
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ''
+      if has_selection then
+        window:perform_action(act.CopyTo('ClipboardAndPrimarySelection'), pane)
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act { PasteFrom = 'Clipboard' }, pane)
+      end
+    end),
+  },
 }
 
 return config
