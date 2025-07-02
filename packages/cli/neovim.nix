90660f5734b8d2e77c3baadbc5bda4a92fce9a6c
sharpchen <rui.chen.sharp@gmail.com>
2025-07-03 03:34:44 +0800

    chore(nvim): remove nix plugin for neovim

--- packages/cli/neovim.nix
+++ packages/cli/neovim.nix
@@ -7,22 +7,6 @@
 {
   programs.neovim = {
     enable = true;
-    plugins = [
-      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-    ];
-    extraWrapperArgs =
-      let
-        nvim-treesitter-parsers =
-          let
-            nvim-treesitter = pkgs.vimPlugins.nvim-treesitter;
-          in
-          builtins.map (grammar: nvim-treesitter.grammarToPlugin grammar) nvim-treesitter.allGrammars;
-      in
-      [
-        "--set"
-        "NVIM_TREESITTER_PARSERS"
-        (lib.concatStringsSep "," nvim-treesitter-parsers)
-      ];
   };
 
   home.file.".config/nvim" = {
