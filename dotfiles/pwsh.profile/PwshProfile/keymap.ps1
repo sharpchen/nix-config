e3828d3f7b8611b4dab1a64bdbb086b3812d3da7
sharpchen <rui.chen.sharp@gmail.com>
2025-07-25 13:07:09 +0800

    chore(pwsh): update profile

--- dotfiles/pwsh.profile/PwshProfile/keymap.ps1
+++ dotfiles/pwsh.profile/PwshProfile/keymap.ps1
@@ -1,23 +1,24 @@
 Set-PSReadLineOption -EditMode Vi
 
-Set-PSReadLineKeyHandler -Key 'Ctrl+ ' -Function MenuComplete
+Set-PSReadLineKeyHandler -Key 'Ctrl+ ' -Function MenuComplete -ViMode Insert
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Chord 'g,h' -Function GotoFirstNonBlankOfLine -ViMode Command
-Set-PSReadLineKeyHandler -Chord Tab -Function GotoBrace -ViMode Command
-Set-PSReadLineKeyHandler -Chord 'g,l' -Function EndofLine -ViMode Command
+Set-PSReadLineKeyHandler -Key Tab -Function ViGotoBrace -ViMode Command
+Set-PSReadLineKeyHandler -Key Y -Function ViYankToEndOfLine -ViMode Command
+Set-PSReadLineKeyHandler -Chord 'g,l' -Function MoveToEndOfLine -ViMode Command
 Set-PSReadLineKeyHandler -Chord ' ,z' -ViMode Command -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::GotoFirstNonBlankOfLine()
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('(')
-    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::MoveToEndOfLine()
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert(')')
 }
 Set-PSReadLineKeyHandler -Chord ' ,Z' -ViMode Command -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('(')
-    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::MoveToEndOfLine()
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert(')')
 }
 Set-PSReadLineKeyHandler -Chord ' , ' -ViMode Command -ScriptBlock {
