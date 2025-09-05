270a27c0747855c4e1492466dc4e984a7ea1a72d
sharpchen <rui.chen.sharp@gmail.com>
2025-09-05 08:55:28 +0000

    feat(dotnet): manage dotnet variables/alias by nix

--- packages/dev/dotnet.nix
+++ packages/dev/dotnet.nix
@@ -1,13 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  dotnet-combined = (
+    with pkgs.dotnetCorePackages;
+    combinePackages [
+      sdk_9_0
+      # sdk_10_0-bin
+    ]
+  );
+in
 {
   home.packages = with pkgs; [
-    (
-      with dotnetCorePackages;
-      combinePackages [
-        sdk_9_0
-        # sdk_10_0-bin
-      ]
-    )
+    dotnet-combined
     netcoredbg
     csharpier
     roslyn-ls
@@ -17,4 +20,13 @@
     ilspycmd
     csharp-ls
   ];
+
+  home.sessionVariables = {
+    DOTNET_ROOT = "${dotnet-combined}/share/dotnet";
+    DOTNET_PATH = "${dotnet-combined}/bin/dotnet";
+  };
+  home.sessionPath = [ "${config.home.homeDirectory}/.dotnet/tools/" ];
+  home.shellAliases = {
+    dn = "dotnet";
+  };
 }
