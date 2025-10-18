d6b01d64aff5f7d82c20b7971b48e8dca18fd4c7
sharpchen <rui.chen.sharp@gmail.com>
2025-10-19 03:17:14 +0800

    fix(windows): installation script

--- install.ps1
+++ install.ps1
@@ -2,80 +2,69 @@
 
 # This file is for fresh installation on a new windows
 
-function RefreshPath {
-    $env:Path =
-    [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
-    + [IO.Path]::PathSeparator
-    + [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
-}
-
 if (-not (Get-Command scoop -ea SilentlyContinue)) {
     # install scoop
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
-scoop bucket add extras
-scoop bucket add versions
-scoop bucket add nerd-fonts
-scoop bucket add fonts 'https://github.com/KnotUntied/scoop-fonts.git'
-RefreshPath
-#endregion
+if (Get-Command scoop -ErrorAction Ignore) {
+    scoop install git # NOTE: adding bucket requires git
+    scoop bucket add extras
+    scoop bucket add versions
+    scoop bucket add nerd-fonts
+    scoop bucket add fonts 'https://github.com/KnotUntied/scoop-fonts.git'
 
-$scoopPackages = @(
-    'git',
-    'vswhere',
-    'neovim',
-    'bat',
-    'tokei',
-    'pnpm',
-    'adb',
-    'gcc',
-    'ripgrep',
-    'lazygit',
-    'nodejs-lts',
-    'wiztree',
-    '7zip',
-    'fzf',
-    'vscodium',
-    'carnac',
-    'librewolf',
-    'abdownloadmanager',
-    'mpv',
-    'ffmpeg',
-    'yazi',
-    'imagemagick',
-    'poppler',
-    'jq',
-    'fd',
-    'miller',
-    'ouch',
-    'glow',
-    'uutils-coreutils',
-    'sioyek',
-    'pwsh',
-    'powertoys',
-    'ditto',
-    'perl',
-    'scoop-completion'
-)
+    $scoopPackages = @(
+        'vswhere',
+        'neovim',
+        'bat',
+        'tokei',
+        'pnpm',
+        'adb',
+        'gcc',
+        'ripgrep',
+        'lazygit',
+        'nodejs-lts',
+        'wiztree',
+        '7zip',
+        'fzf',
+        'vscodium',
+        'carnac',
+        'librewolf',
+        'abdownloadmanager',
+        'mpv',
+        'ffmpeg',
+        'yazi',
+        'imagemagick',
+        'poppler',
+        'jq',
+        'fd',
+        'uutils-coreutils',
+        'sioyek',
+        'pwsh',
+        'powertoys',
+        'ditto',
+        'perl',
+        'scoop-completion',
+        'less',
+        'wezterm',
+        'eartrumpet',
+        'paint.net'
+    )
 
-$scoopFonts = @(
-    'Cascadia-Code',
-    'jetbrainsmono-nl',
-    'notosansmono'
-)
+    $scoopFonts = @(
+        'Cascadia-Code',
+        'jetbrainsmono-nl',
+        'notosansmono',
+        'ibmplexmono',
+        'robotomono-variable'
+    )
 
-$wingetPackages = @(
-    'wez.wezterm',
-    'File-New-Project.EarTrumpet',
-    'dotPDN.PaintDotNet'
-)
+    scoop install @scoopPackages
+    scoop install @scoopFonts
+}
 
-& ./dotfiles.ps1
-
-scoop install @scoopPackages
-scoop install @scoopFonts
-winget add --silent --accept-source-agreements @wingetPackages
-
-RefreshPath
+if (Test-Path ./dotfiles.ps1) {
+    & ./dotfiles.ps1
+}
