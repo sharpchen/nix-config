425356973df09249580e7b8d387f53435d3234b7
sharpchen <rui.chen.sharp@gmail.com>
2026-01-13 18:04:52 +0800

    refactor(windows): remove redundant wrap in install script

--- install.ps1
+++ install.ps1
@@ -4,66 +4,67 @@
 
 if (-not (Get-Command scoop -ea SilentlyContinue)) {
     # install scoop
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -ErrorAction Stop
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression -ErrorAction Stop
 }
 
-if (Get-Command scoop -ErrorAction Ignore) {
-    scoop install git # NOTE: adding bucket requires git
-    scoop bucket add extras
-    scoop bucket add versions
-    scoop bucket add nerd-fonts
-    scoop bucket add fonts 'https://github.com/KnotUntied/scoop-fonts.git'
+scoop install git # NOTE: adding bucket requires git
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add nerd-fonts
+scoop bucket add fonts 'https://github.com/KnotUntied/scoop-fonts.git'
+scoop bucket add dorado https://github.com/chawyehsu/dorado
+scoop alias add add 'scoop install @args'
+scoop alias add del 'scoop uninstall @args'
 
-    $scoopPackages = @(
-        'vswhere',
-        'neovim',
-        'bat',
-        'tokei',
-        'pnpm',
-        'adb',
-        'gcc',
-        'ripgrep',
-        'lazygit',
-        'nodejs-lts',
-        'wiztree',
-        '7zip',
-        'fzf',
-        'vscodium',
-        'carnac',
-        'librewolf',
-        'abdownloadmanager',
-        'mpv',
-        'ffmpeg',
-        'yazi',
-        'imagemagick',
-        'poppler',
-        'jq',
-        'fd',
-        'uutils-coreutils',
-        'sioyek',
-        'pwsh',
-        'powertoys',
-        'ditto',
-        'perl',
-        'scoop-completion',
-        'less',
-        'wezterm',
-        'eartrumpet',
-        'paint.net'
-    )
+$scoopPackages = @(
+    'vswhere',
+    'neovim',
+    'bat',
+    'tokei',
+    'pnpm',
+    'adb',
+    'gcc',
+    'ripgrep',
+    'lazygit',
+    'nodejs-lts',
+    'wiztree',
+    '7zip',
+    'fzf',
+    'vscodium',
+    'carnac',
+    'librewolf',
+    'abdownloadmanager',
+    'mpv',
+    'ffmpeg',
+    'yazi',
+    'imagemagick',
+    'poppler',
+    'jq',
+    'fd',
+    'uutils-coreutils',
+    'sioyek',
+    'pwsh',
+    'powertoys',
+    'ditto',
+    'perl',
+    'scoop-completion',
+    'less',
+    'wezterm',
+    'eartrumpet',
+    'paint.net'
+)
 
-    $scoopFonts = @(
-        'Cascadia-Code',
-        'jetbrainsmono-nl',
-        'notosansmono',
-        'ibmplexmono',
-        'robotomono-variable'
-    )
+$scoopFonts = @(
+    'Cascadia-Code',
+    'jetbrainsmono-nl',
+    'notosansmono',
+    'ibmplexmono',
+    'robotomono-variable'
+)
 
-    scoop install @scoopPackages
-    scoop install @scoopFonts
-}
+scoop install @scoopPackages
+scoop install @scoopFonts
 
 if (Test-Path ./dotfiles.ps1) {
     & ./dotfiles.ps1
