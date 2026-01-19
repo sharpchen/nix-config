9d5c382e5e13b1d8c8e1465394be25acaaa4458f
sharpchen <rui.chen.sharp@gmail.com>
2026-01-19 23:17:43 +0800

    chore(windows): update dotfiles.ps1

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -44,7 +44,7 @@ function mklink {
 
 if (Get-Command scoop -ea SilentlyContinue) {
     if (& { scoop prefix librewolf *> $null; 0 -eq $LASTEXITCODE }) {
-        mklink (Join-Path (scoop prefix librewolf) 'Profiles/Default/librewolf.overrides.cfg') ./dotfiles/librewolf.cfg
+        mklink (Join-Path (scoop prefix librewolf) 'Profiles/Default/librewolf.overrides.cfg') $PSScriptRoot/dotfiles/librewolf.cfg
     }
 
     if (& { scoop prefix git *> $null; 0 -eq $LASTEXITCODE }) {
@@ -52,8 +52,8 @@ if (Get-Command scoop -ea SilentlyContinue) {
     }
 
     if (& { scoop prefix sioyek *> $null; 0 -eq $LASTEXITCODE }) {
-        mklink (Join-Path (scoop prefix sioyek) 'prefs_user.config') ./dotfiles/sioyek.prefs_user.config
-        mklink (Join-Path (scoop prefix sioyek) 'keys_user.config') ./dotfiles/sioyek.keys_user.config
+        mklink (Join-Path (scoop prefix sioyek) 'prefs_user.config') $PSScriptRoot/dotfiles/sioyek.prefs_user.config
+        mklink (Join-Path (scoop prefix sioyek) 'keys_user.config') $PSScriptRoot/dotfiles/sioyek.keys_user.config
     }
 }
 
@@ -62,55 +62,55 @@ if (Get-Command nvim -ea SilentlyContinue) {
     if (Test-Path $nvimConfig) {
         Remove-Item -Recurse $nvimConfig
     }
-    mklink $nvimConfig ./dotfiles/nvim-config
+    mklink $nvimConfig $PSScriptRoot/dotfiles/nvim-config
 }
 
 # git
-mklink ~/.gitconfig ./dotfiles/.gitconfig
-mklink ~/.windows.gitconfig. ./dotfiles/.windows.gitconfig
-mklink -SpecialParent LOCALAPPDATA -ChildPath 'lazygit/config.yml' -Target ./dotfiles/lazygit.config.yml
+mklink ~/.gitconfig $PSScriptRoot/dotfiles/.gitconfig
+mklink ~/.windows.gitconfig. $PSScriptRoot/dotfiles/.windows.gitconfig
+mklink -SpecialParent LOCALAPPDATA -ChildPath 'lazygit/config.yml' -Target $PSScriptRoot/dotfiles/lazygit.config.yml
 
 # vim
-mklink ~/.vimrc ./dotfiles/.vimrc
-mklink ~/.keymap.vim ./dotfiles/.keymap.vim
-mklink ~/.vsvimrc ./dotfiles/.vsvimrc
-mklink ~/.ideavimrc ./dotfiles/.ideavimrc
+mklink ~/.vimrc $PSScriptRoot/dotfiles/.vimrc
+mklink ~/.keymap.vim $PSScriptRoot/dotfiles/.keymap.vim
+mklink ~/.vsvimrc $PSScriptRoot/dotfiles/.vsvimrc
+mklink ~/.ideavimrc $PSScriptRoot/dotfiles/.ideavimrc
 
 # shell
-mklink ~/.bashrc ./dotfiles/.bashrc
-mklink ~/.inputrc ./dotfiles/.inputrc
+mklink ~/.bashrc $PSScriptRoot/dotfiles/.bashrc
+mklink ~/.inputrc $PSScriptRoot/dotfiles/.inputrc
 
 # pwsh
-mklink $PROFILE ./dotfiles/pwsh.profile/pwsh.profile.ps1
-mklink (Join-Path (Split-Path $PROFILE) 'PwshProfile') ./dotfiles/pwsh.profile/PwshProfile/
+mklink $PROFILE $PSScriptRoot/dotfiles/pwsh.profile/pwsh.profile.ps1
+mklink (Join-Path (Split-Path $PROFILE) 'PwshProfile') $PSScriptRoot/dotfiles/pwsh.profile/PwshProfile/
 
 # powershell
 $powershellProfile = (powershell -noprofile -nologo -c '$PROFILE')
-mklink  $powershellProfile ./dotfiles/pwsh.profile/pwsh.profile.ps1
-mklink (Join-Path (Split-Path $powershellProfile) 'PwshProfile') ./dotfiles/pwsh.profile/PwshProfile/
+mklink  $powershellProfile $PSScriptRoot/dotfiles/pwsh.profile/pwsh.profile.ps1
+mklink (Join-Path (Split-Path $powershellProfile) 'PwshProfile') $PSScriptRoot/dotfiles/pwsh.profile/PwshProfile/
 
-mklink ~/.config/wezterm/wezterm.lua ./dotfiles/.wezterm.lua
+mklink ~/.config/wezterm/wezterm.lua $PSScriptRoot/dotfiles/.wezterm.lua
 
 # yazi
-mklink -SpecialParent APPDATA -ChildPath 'yazi/config/yazi.toml' -Target ./dotfiles/yazi.toml
-mklink -SpecialParent APPDATA -ChildPath 'yazi/config/keymap.toml' -Target ./dotfiles/yazi.keymap.toml
+mklink -SpecialParent APPDATA -ChildPath 'yazi/config/yazi.toml' -Target $PSScriptRoot/dotfiles/yazi.toml
+mklink -SpecialParent APPDATA -ChildPath 'yazi/config/keymap.toml' -Target $PSScriptRoot/dotfiles/yazi.keymap.toml
 if (Get-Command ya -ErrorAction SilentlyContinue) {
     ya pkg add yazi-rs/plugins:toggle-pane
     ya pkg add yazi-rs/plugins:jump-to-char
     ya pkg add AnirudhG07/rich-preview
 }
 
-mklink ~/.wslconfig ./dotfiles/.wslconfig
-mklink (Join-Path (scoop prefix mpv) 'portable_config/mpv.conf') ./dotfiles/mpv.conf
-mklink (Join-Path (scoop prefix mpv) 'portable_config/input.conf') ./dotfiles/mpv.input.conf
-mklink ~/.fzfrc ./dotfiles/.fzfrc
+mklink ~/.wslconfig $PSScriptRoot/dotfiles/.wslconfig
+mklink (Join-Path (scoop prefix mpv) 'portable_config/mpv.conf') $PSScriptRoot/dotfiles/mpv.conf
+mklink (Join-Path (scoop prefix mpv) 'portable_config/input.conf') $PSScriptRoot/dotfiles/mpv.input.conf
+mklink ~/.fzfrc $PSScriptRoot/dotfiles/.fzfrc
 
 if (& { scoop prefix vscodium *> $null; 0 -eq $LASTEXITCODE }) {
-    mklink (Join-Path (scoop prefix vscodium) 'data/user-data/User/keybindings.json') ./dotfiles/vscode.keybinds.json
-    mklink (Join-Path (scoop prefix vscodium) 'data/user-data/User/settings.json') ./dotfiles/vscode.settings.json
+    mklink (Join-Path (scoop prefix vscodium) 'data/user-data/User/keybindings.json') $PSScriptRoot/dotfiles/vscode.keybinds.json
+    mklink (Join-Path (scoop prefix vscodium) 'data/user-data/User/settings.json') $PSScriptRoot/dotfiles/vscode.settings.json
 }
 
-mklink ~/.ssh/config ./dotfiles/sshconfig
+mklink ~/.ssh/config $PSScriptRoot/dotfiles/sshconfig
 
 if (& { scoop prefix windows-terminal *> $null; 0 -eq $LASTEXITCODE }) {
     $settings = Join-Path (scoop prefix windows-terminal) 'settings' 'settings.json'
@@ -119,9 +119,9 @@ if (& { scoop prefix windows-terminal *> $null; 0 -eq $LASTEXITCODE }) {
         # scoop would probably add profiles(wsl profiles ect) on installation too
         # so if settings.json already exists, we should add those generated profiles
         $generated = Get-Content $settings | ConvertFrom-Json
-        $mySettings = Get-Content ./dotfiles/windows-terminal.settings.json | ConvertFrom-Json
-        $mySettings.profiles.list = $generated.profiles.list
-        $mySettings.defaultProfile = $generated.defaultProfile
-        Set-Content $settings (ConvertTo-Json $mySettings)
+        $mySettings = Get-Content $PSScriptRoot/dotfiles/windows-terminal.settings.json | ConvertFrom-Json
+        $mySettings.profiles | Add-Member -MemberType NoteProperty -Name 'list' -Value $generated.profiles.list
+        $mySettings | Add-Member -MemberType NoteProperty -Name 'defaultProfile' -Value $generated.defaultProfile
+        Set-Content $settings (ConvertTo-Json $mySettings -Depth 100)
     }
 }
